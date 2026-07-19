c848a3113e73d000f75f8ef55af05d68e6923e79
MishaDobrynin <77999860+MishaDobrynin@users.noreply.github.com>
2026-07-19 00:55:37 -0700

    Update CircuitCanvas.java
    call gridrenderer

--- src/gui/CircuitCanvas.java
+++ src/gui/CircuitCanvas.java
@@ -1,14 +1,18 @@
 package gui;
+
 import gui.render.GridRenderer;
 import gui.camera.Camera;
+import javafx.beans.value.ChangeListener;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
 public class CircuitCanvas extends Pane{
     private final Canvas backgroundCanvas;
     private final Camera camera;
     private final GridRenderer gridRenderer;
-    
+
     public CircuitCanvas(Camera camera){
         this.camera = camera;
         gridRenderer = new GridRenderer(camera);
@@ -18,5 +22,28 @@ public class CircuitCanvas extends Pane{
         backgroundCanvas.heightProperty().bind(heightProperty());
 
         getChildren().add(backgroundCanvas);
+
+        ChangeListener<Number> resizeListener = (obs, oldVal, newVal) -> redraw();
+
+        widthProperty().addListener(resizeListener);
+        heightProperty().addListener(resizeListener);
+
+        redraw();
+    }
+
+    private void redraw(){
+        GraphicsContext gc = backgroundCanvas.getGraphicsContext2D();
+
+        double width = backgroundCanvas.getWidth();
+        double height = backgroundCanvas.getHeight();
+
+        gc.setFill(Color.WHITE); //clears previous frame
+        gc.fillRect(0,0,width,height);
+        
+        gridRenderer.render( //grid draw
+                gc,
+                width,
+                height
+        );
     }
 }
