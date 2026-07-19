18d2f2843707c552d634ab1225c0160d4c39d728
MishaDobrynin <77999860+MishaDobrynin@users.noreply.github.com>
2026-07-19 00:31:45 -0700

    Update Camera.java
    removed comments present in readme.md + spacing

--- src/gui/camera/Camera.java
+++ src/gui/camera/Camera.java
@@ -1,44 +1,41 @@
 package gui.camera;
 import util.Vector2;
 public class Camera {
-    /**
-     * this class is an abstraction of the camera
-     * it will not be doing any panning
-     * simply coordinate processing (is the plan)
-     * and information storage
-     */
     private Vector2 center;
     private double zoom;
-    public static final double DEFAULT_ZOOM = 1.0; //zooming default
+    public static final double DEFAULT_ZOOM = 1.0; //zoom default
+    
     public Camera(){
         center = new Vector2(0, 0);
         zoom = DEFAULT_ZOOM;
     }
+    
     public Vector2 getCenter(){
         return this.center;
     }
+    
     public double getZoom(){
         return this.zoom;
     }
+    
     public void setCenter(Vector2 center){
         this.center = center;
     }
+    
     public void move(double dx, double dy){
-        center = new Vector2(
+        this.center = new Vector2(
                 center.getX() + dx,
                 center.getY() + dy
         );
     }
+    
     public void setZoom(double zoom){
         if(zoom <= 0){
             throw new IllegalArgumentException ("Zoom must be positive.");
         }
         this.zoom = zoom;
     }
-    /**
-     * convert world position
-     * into camera-relative coordinates
-     */
+    
     public Vector2 worldToScreen(Vector2 world, double viewportWidth, double viewportHeight){
         Vector2 relative = world.subtract(center); //where the item is relative to the center
         relative = relative.multiply(zoom); //account for zooming
@@ -48,6 +45,7 @@ public class Camera {
                 )
         );
     }
+    
     public Vector2 screenToWorld(Vector2 screen, double viewportWidth, double viewportHeight){ //process mouse click data
         Vector2 relative = screen.subtract(
                 new Vector2(
