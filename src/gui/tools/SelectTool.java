79896ff87b85930402ff43c214e4d2b1c6410d35
MishaDobrynin <77999860+MishaDobrynin@users.noreply.github.com>
2026-07-21 23:47:51 -0700

    define SelectTool.java
    framework for selecting things on the screen
    -receive mouse events
    -convert screen coordinates into world coordinates
    -print or otherwise expose where the user clicked

--- src/gui/tools/SelectTool.java
+++ src/gui/tools/SelectTool.java
@@ -1,4 +1,25 @@
 package gui.tools;
 
-public class SelectTool{
+import gui.CircuitCanvas;
+import javafx.scene.input.MouseEvent;
+import util.Vector2;
+
+/**
+ * Default editor tool.
+ *
+ * Handles selecting and moving circuit objects.
+ */
+public class SelectTool extends Tool {
+
+    @Override
+    public void mousePressed(MouseEvent event, CircuitCanvas canvas) {
+
+        Vector2 worldPosition = canvas.getCamera().screenToWorld(
+                new Vector2(event.getX(), event.getY()),
+                canvas.getWidth(),
+                canvas.getHeight()
+        );
+
+        System.out.println("Clicked at world position: " + worldPosition);
+    }
 }
