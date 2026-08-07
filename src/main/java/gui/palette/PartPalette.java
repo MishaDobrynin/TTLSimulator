186429503b6c3a0b36d2a2cf41c25d000c977e95
MishaDobrynin <77999860+MishaDobrynin@users.noreply.github.com>
2026-08-07 00:06:32 -0700

    Update PartPalette.java
    assumes existence of part factory class.

--- src/main/java/gui/palette/PartPalette.java
+++ src/main/java/gui/palette/PartPalette.java
@@ -1,4 +1,95 @@
 package gui.palette;
 
+import gui.CircuitCanvas;
+import gui.tools.PartFactory;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
 public class PartPalette {
+    private static final double SIZE = 50;
+
+    private final Canvas canvas;
+    private final CircuitCanvas circuitCanvas;
+
+    private double centerX;
+    private double centerY;
+
+    public PartPalette(CircuitCanvas circuitCanvas){
+        this.circuitCanvas = circuitCanvas;
+
+        canvas = new Canvas(150, 150);
+
+        canvas.setVisible(false);
+
+        canvas.setOnMouseClicked(event -> {
+            handleClick(event.getX(), event.getY());
+            event.consume();
+        });
+
+        draw();
+    }
+
+    public Canvas getCanvas(){
+        return canvas;
+    }
+
+    public void open(double x, double y){
+        centerX = x;
+        centerY = y;
+
+        canvas.setLayoutX(centerX - 75);
+        canvas.setLayoutY(centerY - 75);
+
+        canvas.setVisible(true);
+
+        draw();
+    }
+
+    public void close(){
+        canvas.setVisible(false);
+    }
+
+    private void handleClick(double x, double y){
+        int column = (int)(x / SIZE);
+        int row = (int)(y / SIZE);
+
+        int index = getPartIndex(row, column);
+
+        if(index != -1){
+            circuitCanvas.getToolManager().setCurrentTool(
+                    PartFactory.getPart(index),
+                    circuitCanvas
+            );
+        }
+
+        close();
+    }
+
+    private int getPartIndex(int row, int column){
+        return switch(row * 3 + column){
+            case 0 -> 1; // NAND
+            case 1 -> 2; // NOT
+
+            default -> -1;
+        };
+    }
+
+    private void draw(){
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+        gc.setStroke(Color.BLACK);
+
+        for(int i = 0; i <= 3; i++){
+            gc.strokeLine(i * SIZE, 0, i * SIZE, 150);
+            gc.strokeLine(0, i * SIZE, 150, i * SIZE);
+        }
+
+        gc.strokeText("NAND", 6, 30);
+        gc.strokeText("NOT", 60, 30);
+
+        gc.strokeText("+", 72, 80);
+    }
 }
